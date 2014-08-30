0a9c22a905c7b6df79bf7247bd66170e09b15d6e
Alex Hughes <ahughes@ahughes.org>
2014-08-30 01:22:04 +0100

    Added very simple one table DB along with java project skeleton

---
+++ java/monies/src/monies/Main.java
@@ -0,0 +1,19 @@
+
+package monies;
+
+/**
+ * The Main Class that instantiates the LoginFrame of the application
+ * Shall you have any queries, drop me a line.
+ * 
+ * @author ahughes <ahughes@ahughes.org>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
